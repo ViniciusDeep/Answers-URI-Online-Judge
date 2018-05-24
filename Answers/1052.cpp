74b7bd53c6881729b8988f8e3feabb281d69160c
Vinicius Deep <viniciuscorreia11@gmail.com>
2018-05-24 11:00:22 -0300

    Add answer of problem 1052

---
+++ Answers/1052.cpp
@@ -0,0 +1,58 @@
+ #include <stdio.h>
+ 
+   int main() {
+ 
+	int x;
+	
+	scanf("%d",&x);
+	
+	if (x == 1) {
+		printf("January\n");
+	}
+	
+	else if (x == 2) {
+		printf("February\n");
+	}
+	
+	else if (x == 3) {
+		printf("March\n");
+	}
+	
+	else if (x == 4) {
+		printf("April\n");
+	}
+	
+	else if (x == 5) {
+		printf("May\n");
+	}
+	
+	else if (x == 6) {
+		printf("June\n");
+	}
+	
+	else if (x == 7) {
+		printf("July\n");
+	}
+	
+	else if (x == 8) {
+		printf("August\n");
+	}
+	
+	else if (x == 9) {
+		printf("September\n");
+	}
+	
+	else if (x == 10) {
+		printf("October\n");
+	}
+	
+	else if (x == 11) {
+		printf("November\n");
+	}
+	
+	else if (x == 12) {
+		printf("December\n");
+	}
+	
+	return 0;
+ }
