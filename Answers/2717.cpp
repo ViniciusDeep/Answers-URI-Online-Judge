b1ccbf4d56bf41731d250dc6a126f9c5209b0258
Vinicius Deep <viniciuscorreia11@gmail.com>
2018-05-31 19:10:19 -0300

    Add solve to 2717

---
+++ Answers/2717.cpp
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  int n, a, b;
+  scanf("%d%d%d", &n, &a, &b);
+  (a + b > n) ? puts("Deixa para amanha!") : puts("Farei hoje!");
+  return 0;
+}
