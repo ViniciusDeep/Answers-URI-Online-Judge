fbfca314fa16727975119e10fa9350f62157dce4
hugo galvao <hugoaddobbatigalvao@hotmail.com>
2019-10-30 20:17:05 -0300

    1050 python

---
+++ Answers/Python/1050.py
@@ -0,0 +1,19 @@
+a=int(input())
+if a==61:
+    print("Brasilia")
+elif a==71:
+    print("Salvador")
+elif a==11:
+    print("Sao Paulo")
+elif a==21:
+    print("Rio de Janeiro")
+elif a==32:
+    print("Juiz de Fora")
+elif a==19:
+    print("Campinas")
+elif a==27:
+    print("Vitoria")
+elif a==31:
+    print("Belo Horizonte")
+else:
+    print("DDD nao cadastrado")
